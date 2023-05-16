1c8f54e6e71ae64597ebe290041f284cf2ea7fcd
Julien <julien.bonyadi@gmail.com>
2023-05-16 10:06:06 +0200

    front julien

--- screens/ProfilScreen.js
+++ screens/ProfilScreen.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import { Button, StyleSheet, Text, View } from 'react-native';
+import Inputs from '../components/Inputs';
 
 export default function ProfilScreen({ navigation }) {
+
+
  return (
-   <View>
+   <View style={styles.container}>
     <Text>ProfilScreen</Text>
+    <Inputs/>
    </View>
  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+     flex:1,
+     paddingTop:100,
+      alignItems:"center",
+      justifyContent:"space-between",
+      backgroundColor:'#242424',
+  }})
