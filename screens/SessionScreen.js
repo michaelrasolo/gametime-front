1359ed725cefdd333d515ea2c266b4e34d2abb8f
Pierre-Baptiste <pierrebaptiste.rozic@gmail.com>
2023-05-16 10:01:21 +0200

    pba

--- screens/SessionScreen.js
+++ screens/SessionScreen.js
@@ -1,21 +1,11 @@
 import React from 'react';
 import { Button, StyleSheet, Text, View } from 'react-native';
-import SearchBar from '../components/SearchBar';
 
-export default function SessionScreen({ navigation }) {
+export default function SearchScreen({ navigation }) {
  return (
-   <View style={styles.container}>
-    <SearchBar name="Saisis ta ville"/>
-    <Text>SessionScreen</Text>
+   <View>
+    <Text>SearchScreen</Text>
    </View>
  );
 }
 
-
-const styles = StyleSheet.create({
-  container: {
-     flex:1,
-     paddingTop:100,
-      alignItems:"center",
-      justifyContent:"space-between"
-  }})
