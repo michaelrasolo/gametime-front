d429954f405c908d9880228360b5e6b2d6fbc55a
Julien <julien.bonyadi@gmail.com>
2023-05-22 09:40:34 +0200

    julien

--- screens/SignInScreen.js
+++ screens/SignInScreen.js
@@ -16,7 +16,7 @@ export default function SignInScreen({ navigation }) {
 
   const handleConnection = () => {
     // navigation.navigate('TabNavigator')
-    fetch('http://192.168.10.151:3000/users/signin', {
+    fetch('http://192.168.0.103:3000/users/signin', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email, password }),
